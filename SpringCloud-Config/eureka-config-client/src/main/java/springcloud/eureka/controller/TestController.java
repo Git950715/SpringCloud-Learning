29093be650a8b57ccee3cbc0b51ed704395afafa
Git950715 <965527916@qq.com>
2019-10-14 22:29:08 +0800

    config配置中心初始化

---
+++ SpringCloud-Config/eureka-config-client/src/main/java/springcloud/eureka/controller/TestController.java
@@ -0,0 +1,18 @@
+package springcloud.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@Value("${name}")
+	private String name;
+	
+	@RequestMapping(value="/api/config/test")
+	public String backConfig(){
+		return name;
+	}
+	
+}
