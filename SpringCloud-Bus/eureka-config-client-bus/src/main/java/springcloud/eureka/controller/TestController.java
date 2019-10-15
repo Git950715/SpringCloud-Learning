3bb1a0fdf44e6e7e36ff26947aeba75f870a355c
Git950715 <965527916@qq.com>
2019-10-15 20:00:09 +0800

    消息总线bus初始化

---
+++ SpringCloud-Bus/eureka-config-client-bus/src/main/java/springcloud/eureka/controller/TestController.java
@@ -0,0 +1,20 @@
+package springcloud.eureka.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class TestController {
+
+	@Value("${name}")
+	private String name;
+	
+	@RequestMapping(value="/api/config/test")
+	public String backConfig(){
+		return name;
+	}
+	
+}
